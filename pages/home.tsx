f2ca2a1bfb5500f158342bdb53f98d56f8efe71c
Steve <steven.ballaret-17@cpu.edu.ph>
2022-12-05 18:14:23 +0800

    ola

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,16 +1,15 @@
 function Home() {
   return  (
-   
     <nav className="navbar navbar-expand-md navbar-light bg-light ">
        <div className="container-fluid">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <button className="nav-link badge bg-warning text-light"> Add Container </button>
-          </li>
-          <li className="nav-item">
-            <button className="nav-link badge bg-warning text-light">Log out</button>
-          </li>
-        </ul>
+        
+          <div className="nav-item">
+            <button className="nav-link badge bg-warning text-light w-100"> Add Container </button>
+          </div>
+          <div className="nav-item">
+            <button className="nav-link badge bg-warning text-light w-100">Log out</button>
+          </div>
+        
       </div>
     </nav>
     
