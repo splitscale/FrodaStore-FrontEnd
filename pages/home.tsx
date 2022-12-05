5637a05091e824cc0508ae1c1d63ff8833faf7ff
Steve <steven.ballaret-17@cpu.edu.ph>
2022-12-05 18:15:56 +0800

    a

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,17 +1,27 @@
+import Link from "next/link";
+
 function Home() {
   return  (
-    <nav className="navbar navbar-expand-md navbar-light bg-light ">
-       <div className="container-fluid">
-        
-          <div className="nav-item">
-            <button className="nav-link badge bg-warning text-light w-100"> Add Container </button>
-          </div>
-          <div className="nav-item">
-            <button className="nav-link badge bg-warning text-light w-100">Log out</button>
-          </div>
-        
+    <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
+      <div className="container-fluid">
+        <img src='main-logo.png' className='img-thumbnail border-white' alt='main-logo.png' />
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#"> Add Container</a>
+          </li>
+          <Link href='/'>
+            <li className="nav-item">
+              <a className="nav-link">Log out</a>
+            </li>
+          </Link>
+        </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
     
   )
 }
